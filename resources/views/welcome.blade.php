d3e6365381d647b73478cc75eba25f15f5b05356
IsabelLinan <L18180130@slp.tecnm.mx>
2022-05-16 18:17:04 -0500

    Modificacion 12-05-22

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <h1 style="color: white">Servidores  2022 Erika </h1>
+                    <h2>Hola Mundo</h2>
                     
                 </div>
 
